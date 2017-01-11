1444be72a1e2d996f759bd2f0ce9fb0558212f45
Boris <boris.pozzo@gmail.com>
2017-01-11 11:25:26 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+elem = document.getElementById("module-1");
+elem2 = document.getElementById("module-2");
+elem3 = document.getElementById("module-3");
+elem4 = document.getElementById("module-4");
+elem5 = document.getElementById("module-5");
+elem6 = document.getElementById("module-6");
+
+
+	elem.onclick = function play() {
+		audio = document.getElementById('player1');
+		audio.play();
+	}
+
+	elem2.onclick = function play2() {
+		audio2 = document.getElementById('player2');
+		audio2.play();
+	}
+
+	elem3.onclick = function play3() {
+		audio3 = document.getElementById('player3');
+		audio3.play();
+	}
+
+	elem4.onclick = function play4() {
+		audio4 = document.getElementById('player4');
+		audio4.play();
+	}
+
+	elem5.onclick = function play5() {
+		audio5 = document.getElementById('player5');
+		audio5.play();
+	}
+
+	elem6.onclick = function play6() {
+		audio6 = document.getElementById('player6');
+		audio6.play();
+	}
+	
+
+	
